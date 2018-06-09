c85751737fec7fc5dcf295a110b5463f8cd9d8a7
pc035860 <pc035860@gmail.com>
2018-06-09 17:30:39 +0800

    Bump version 0.3.0
    - add .editorconfig
    - update README.md

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,20 +8,21 @@ var mountFolder = function (connect, dir) {
 
 module.exports = function(grunt) {
   grunt.initConfig({
+    fileName: 'ngQueue',
     docRoot: DOC_ROOT,
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
       options: {
         curly: true
       },
-      beforeuglify: ['<%= pkg.name %>.js'],
+      beforeuglify: ['<%= fileName %>.js'],
       gruntfile: ['Gruntfile.js']
     },
     umd: {
       build: {
         options: {
-          src: '<%= pkg.name %>.js',
-          dest: '<%= pkg.name %>.umd.js',
+          src: '<%= fileName %>.js',
+          dest: '<%= fileName %>.umd.js',
 
           deps: {
             'default': ['angular']
@@ -31,14 +32,14 @@ module.exports = function(grunt) {
     },
     uglify: {
       build: {
-        src: '<%= pkg.name %>.umd.js',
-        dest: '<%= pkg.name %>.min.js'
+        src: '<%= fileName %>.umd.js',
+        dest: '<%= fileName %>.min.js'
       },
       options: {
         mangle: true,
         compress: {},
         banner: 
-          '/*! <%= pkg.name %>\n' + 
+          '/*! <%= fileName %>\n' + 
           'version: <%= pkg.version %>\n' +
           'build date: <%= grunt.template.today("yyyy-mm-dd") %>\n' + 
           'author: <%= pkg.author %>\n' + 
@@ -46,12 +47,12 @@ module.exports = function(grunt) {
       }
     },
     clean: {
-      build: ['<%= pkg.name %>.umd.js']
+      build: ['<%= fileName %>.umd.js']
     },
     copy: {
       example: {
-        src: '<%= pkg.name %>.js',
-        dest: '<%= docRoot %>/lib/<%= pkg.name %>.js'
+        src: '<%= fileName %>.js',
+        dest: '<%= docRoot %>/lib/<%= fileName %>.js'
       }
     },
     watch: {
@@ -60,7 +61,7 @@ module.exports = function(grunt) {
         tasks: ['jshint:gruntfile'],
       },
       src: {
-        files: '<%= pkg.name %>.js',
+        files: '<%= fileName %>.js',
         tasks: ['newer:jshint:beforeuglify', 'copy:example'],
       },
       livereload: {
@@ -68,7 +69,7 @@ module.exports = function(grunt) {
           livereload: LIVERELOAD_PORT
         },
         files: [
-          '<%= pkg.name %>.js',
+          '<%= fileName %>.js',
           '<%= docRoot %>/{,**/}*.{html,js,css}'
         ]
       }
